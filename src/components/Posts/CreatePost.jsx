47c4adb93ff28772595770f8526aead3f1e6c0dd
Tyson McLeod <tysonmcleod@gmail.com>
2021-09-15 15:44:02 +0200

    Group functionality for a user when creating a post. Dynamically add a users groups in to create post's group list choice as an option

--- src/components/Posts/CreatePost.jsx
+++ src/components/Posts/CreatePost.jsx
@@ -1,34 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 import { useForm } from "react-hook-form";
 import { addPost } from "../../services/api/posts";
 import { useHistory } from 'react-router-dom'
+import { getUsersGroups } from "../../services/api/group";
 
-const CreatePost = ( props ) => {
+const CreatePost = () => {
+    const [groupObjects, setGroupObjects] = useState([]);   
+    /**
+     * TODO: Check if user is logged in 
+     */
+    useEffect(() => {
+        getGroupList();
+    }, []);
 
     const [input, setInput] = useState();
     const [showPreview, setShowPreview] = useState(true);
     const { register, handleSubmit /*,formState: { errors } */} = useForm();
     const history = useHistory();
 
-   // TODO: create function which gets a users groups and adds them to group object
-    
-   // Group objects -> used to create menu and display name 
-    const groupObject = [
-        {
-            name: "Duis consequat dui nec nisi volutpat eleifend.",
-            id: 2
-        },
-        {
-            name: "Test example",
-            id: 999
-        }
-    ]
 
-    // TODO: create function which uses a groups id and appends it to 
-    // the data object as "receiverID" and also adds receiver type
-    //  which is then sent as a post request to the api
+
+    const getGroupList = async () => {
+        try {
+          const data = await getUsersGroups(2); // set this to user id 
+          setGroupObjects(data);
+        } catch (error) {
+          console.error("Error:", error);
+        }
+    };
+
 
     const onSubmit = (data) => {
         data.topic = {
@@ -37,7 +39,7 @@ const CreatePost = ( props ) => {
         data.receiverType = "group"
         console.log(data);
         createPost(data);
-        history.push('/groups/2');
+        history.push('/groups/2'); // change this to the actual post when view thread is complete
     }
 
 
@@ -46,17 +48,23 @@ const CreatePost = ( props ) => {
     }
 
 
+    const getListOptions = groupObjects
+    .map((listOption) => (
+      <option key={listOption.id} value={listOption.id}>{listOption.name}</option>
+    ));
+
+
+
    return (
 
         <div className="postPage">
-            <h1>Hello world, this is create a post page with some amazing css</h1>
+            <h1>Create post</h1>
 
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input type="text" placeholder="Title"
                             {...register("title")} 
                         />
 
-               
                 <select {...register("topic")}>
                     <option value="1">Sports</option>
                     <option value="2">Lifestyle</option>
@@ -66,13 +74,11 @@ const CreatePost = ( props ) => {
                 </select>
                 
                 <select {...register("receiverId")}>
-                    <option value={groupObject[0].id}>{groupObject[0].name}</option>
+                    {getListOptions}
                 </select>
 
                 <textarea autoFocus className="textarea" value={input} {...register("content")} 
                     onChange= {(e) => setInput(e.target.value)}
-                    
-                    
                 />
                 <input className="postPageSubmit" type="submit" />
             </form>
@@ -81,8 +87,6 @@ const CreatePost = ( props ) => {
                     {
                         showPreview ? <ReactMarkdown remarkPlugins={[gfm]} className="markdown" children={input} />  : null  
                     }
-            
-            
         </div>
     )
 }
--- src/services/api/group.js
+++ src/services/api/group.js
@@ -1,4 +1,5 @@
 const BASE_URL = "https://alumni-network-backend.herokuapp.com/api/v1/";
+const BASE_USER_URL = "https://alumni-network-backend.herokuapp.com/api/v1/user/";
 
 /**
  * get them groups 
@@ -10,6 +11,52 @@ export const getPublicGroups = async () => {
     const data = await response.json();
     return data.filter(x => x.private === false);
 }
+
+
+/**
+ * Get the groups that a user is in 
+ */
+export const getUsersGroups = async (userId) => {
+    const group_urls = await getUserGroupsList(userId);
+    const fake_group_urls = ['/api/v1/group/2', '/api/v1/group/7']  // while api is down 
+    return fetchAll(fake_group_urls);
+}
+
+/**
+ * Get list of user groups 
+ */
+ export const getUserGroupsList = async (userId) => {
+    const USER_URL = BASE_USER_URL + userId;
+    const response = await fetch(USER_URL);
+    const data = await response.json();
+    return data.groups 
+}
+
+
+/**
+ * Transform group data in to objects with name and id
+ * for usage in createPost when getting groups 
+ */
+
+const processGroupData = (data) => {
+    let dataTransform = [];
+    for (let i = 0; i < data.length; ++i) {
+        dataTransform.push({
+            name: data[i].name,
+            id: data[i].id
+        });
+    }
+    return dataTransform;
+}
+
+// helper function to fetch multiple urls 
+
+const fetchAll = async (urls) => {
+    const response = await Promise.all(urls.map(u => fetch("https://alumni-network-backend.herokuapp.com"+u)));
+    const data = await Promise.all(response.map(r => r.json()));
+    return processGroupData(data);
+}
+
 
 /**
  * get a group big boy
