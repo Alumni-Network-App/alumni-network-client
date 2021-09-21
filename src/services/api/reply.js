64f4d7d44524055f24cbca62a5d7919535d5fcaa
Robin Eliasson <robineliasson93@gmail.com>
2021-09-21 08:48:00 +0200

    Some more reply fetching

--- src/services/api/reply.js
+++ src/services/api/reply.js
@@ -12,7 +12,7 @@ export const getRepliesToPost = async (postId) => {
             Authorization: `Bearer ${accessToken}`
         }
     });
-    if(!response.ok) throw new Error ("Oops! Something went wrong.");
+    if(!response.ok) throw new Error ("Oops! Error fetching replies.");
     else {
         const data = await response.json();
         return data;
@@ -31,4 +31,20 @@ export const createReply = async (postId, reply) => {
           },
         body: JSON.stringify(reply)
     });
+    if(!response.ok) throw new Error ("Oops! Error creating reply.");
+}
+
+export const updateReply = async (replyId, reply) => {
+    const accessToken = await auth.currentUser.getIdToken(true).then((idToken) => idToken);
+    const REPLY_URL = BASE_REPLY_URL + replyId;
+    const response = await fetch(REPLY_URL, {
+        method: "PUT",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+          },
+        body: JSON.stringify(reply)
+    });
+    if(!response.ok) throw new Error ("Oops! Error updating reply.");
 }
